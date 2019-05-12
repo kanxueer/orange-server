44f917dfff36c963b6d28161ba0b4ab7472ddeb8
kanxueer <kanxueer@gmail.com>
2019-05-12 14:17:34 +0800

    add contract module

---
+++ contract/src/main/java/OrangeContract.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public class OrangeContract {
+
+}
+
+class KVParameter<K, V>{
+    K key;
+    V value;
+}
+
+class OrangeActiveRequest{
+    List<KVParameter> parameters;
+}
+
+class getActiveResponse{
+
+}
