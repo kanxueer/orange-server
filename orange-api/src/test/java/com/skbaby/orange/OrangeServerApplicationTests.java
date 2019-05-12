47d8fd6cc1b8bc2f9a1803224aa44c0ea956cd63
kanxueer <kanxueer@gmail.com>
2019-05-12 13:40:20 +0800

    init project

---
+++ orange-api/src/test/java/com/skbaby/orange/OrangeServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.skbaby.orange;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrangeServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
