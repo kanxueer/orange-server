c02d06eb791d5b38409de9a9eefb71756dcd9402
kanxueer <kanxueer@gmail.com>
2019-07-31 21:47:51 +0800

    update code

--- orange-api/src/main/java/com/skbaby/orange/OrangeServerApplication.java
+++ orange-api/src/main/java/com/skbaby/orange/OrangeServerApplication.java
@@ -1,11 +1,14 @@
 package com.skbaby.orange;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class OrangeServerApplication {
@@ -17,6 +20,26 @@ public class OrangeServerApplication {
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
 		// Do any additional configuration here
+		System.out.println("aaa");
 		return builder.build();
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder, String abc) {
+		// Do any additional configuration here
+		System.out.println("bbb");
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+		};
 	}
 }
